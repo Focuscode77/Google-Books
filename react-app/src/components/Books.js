94d6a45df2bf8b54caa2eee8350937a63de63d4f
Joshua Coleman <procoleman77@outlook.com>
2020-01-09 18:19:51 -0500

    added google api working in console

--- react-app/src/components/Books.js
+++ react-app/src/components/Books.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import SearchArea from './SearchArea';
+import request from 'superagent';
 
 class Books extends Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
-      searchfield: ''
+      searchField: ''
     };
   }
 
+  searchBook = e => {
+    e.preventDefault();
+
+    request
+      .get('https://www.googleapis.com/books/v1/volumes?q=javascript')
+      .query({ q: this.searchField })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log('caught', err.message);
+      });
+  };
+
   handleSearch = e => {
-    console.log(e.target.value);
     this.setState({ searchField: e.target.value });
   };
 
   render() {
     return (
       <div>
-        <SearchArea handleSearch={this.handleSearch} />
+        <SearchArea
+          searchBook={this.searchBook}
+          handleSearch={this.handleSearch}
+        />
       </div>
     );
   }
